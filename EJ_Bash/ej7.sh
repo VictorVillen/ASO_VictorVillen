e16d6f8ae120114cbbd76903593fe7d7652cbdcb
Victor Villen <vicvilveg@alu.edu.gva.es>
2024-09-18 08:33:51 +0200

    He mejorado el ejercicio 7 con un condicional

--- EJ_Bash/ej7.sh
+++ EJ_Bash/ej7.sh
@@ -2,6 +2,12 @@
 
 directorio=$1
 
+if [[ -d "$directorio" ]]; then
+	echo "El directorio es válido"
+else
+	echo "El directorio no existe o no es un directorio"
+fi
+
 fecha=$(date +"%Y-%m-%d")
 
 nombre_directorio=$(basename "$directorio")
