bc41ac9cf921dee109d7c205579f773c9ea653b7
Victor Villen <vicvilveg@alu.edu.gva.es>
2024-09-19 11:44:29 +0200

    He subido el ejercicio 11 acabado

--- EJ_Bash/ej11.sh
+++ EJ_Bash/ej11.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 
-arch1=$1
-arch2=$2
+archivo_origen=$1
+archivo_destino=$2
 
+if [[ "$#" -ne 2 ]]; then
+	echo "No se han indicado dos parametros"
+	exit 1
+fi
+
+if [[ ! -f "$archivo_origen" ]]; then
+	echo "El archivo de origen no existe"
+	exit 1
+fi
+
+if [[ -e "$archivo_destino" ]]; then 
+	echo "Ya exise un identificador con el nombre del archivo de destino"
+	exit 1
+fi
+
+cp "$archivo_origen" "$archivo_destino"
 
