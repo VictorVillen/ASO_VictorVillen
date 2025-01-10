08520c9c9857733a15ca4ba0756a52df78ae14b5
Victor Villen <vicvilveg@alu.edu.gva.es>
2025-01-10 12:11:13 +0100

    he subido el ejercicio

---
+++ ejercicios_powershell/Practica 3/ej4.ps1
@@ -0,0 +1,38 @@
+﻿Write-host "********* CALCULADORA *********"
+Write-Host ""
+
+$num1 = Read-Host "Introduce el primer número"
+$num2 = Read-Host "Introduce el segundo número"
+
+$num1 = [double]$num1
+$num2 = [double]$num2
+
+write-host "1.Sumar"
+write-host "2.Restar"
+Write-Host "3.Multiplicar"
+Write-Host "4.Dividir"
+Write-Host ""
+
+$opcion = Read-Host "Elige la operacion que quieres realizar (1-4)"
+
+switch ($opcion) {
+    1 {
+        $resultado = $num1 + $num2
+        Write-Host "Resultado de la suma: $resultado"
+    }
+    2 {
+        $resultado = $num1 - $num2
+        Write-Host "Resultado de la resta: $resultado" 
+    }
+    3 {
+        $resultado = $num1 * $num2
+        Write-Host "Resultado de la multiplicación: $resultado" 
+    }
+    4 {
+       $resultado = $num1 / $num2
+            Write-Host "Resultado de la división: $resultado" 
+        }
+    Default {
+        Write-Host "Opción inválida. Selecciona un número entre 1 y 4."
+    }
+}
---
+++ ejercicios_powershell/Practica 3/ej5.ps1
@@ -0,0 +1,7 @@
+﻿$ruta = Read-Host "Introduce la ruta del fichero o carpeta"
+
+if (Test-Path $ruta) {
+    Write-Host "El fichero o carpeta existe." 
+} else {
+    Write-Host "El fichero o carpeta no existe." 
+}
