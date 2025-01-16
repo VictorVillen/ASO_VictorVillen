e471b5a21b328cb137a291db65c318bc6af1a87d
Victor Villen <vicvilveg@alu.edu.gva.es>
2025-01-16 12:50:14 +0100

    he subido el ejercicio

---
+++ ejercicios_powershell/Practica 3/ej6.ps1
@@ -0,0 +1,8 @@
+﻿$ruta = Read-Host "Introduce la ruta del fichero o carpeta"
+
+if (Test-Path -Path $ruta -PathType Container) {
+    Write-Host "La carpeta existe."
+    Get-ChildItem -Path $ruta -Recurse
+} else {
+    Write-Host "La carpeta no existe." 
+}
